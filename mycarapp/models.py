932328592c46f8118eecfc836feaeeac3106cbae
daviti1986 <davitpkhakadze86@gmail.com>
2020-05-28 13:07:33 +0400

    new_app

--- mycarapp/models.py
+++ mycarapp/models.py
@@ -1,3 +1,22 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class MyCar(models.Model):
+
+    set_name = models.CharField(default='-', max_length=30 )
+    name     = models.CharField(max_length=30 )
+    fb       = models.CharField(default= '-', max_length=30 )
+    tw       = models.CharField(default= '-', max_length=30 )
+    yt       = models.CharField(default= '-', max_length=30 )
+    Tell     = models.CharField(default= '-', max_length=30 )
+    Link     = models.CharField(default= '-', max_length=30 )
+    about    = models.TextField()
+
+
+
+    def __str__(self):
+        return self.set_name + ' | ' + str (self.pk)
+
+
--- mycarapp/views.py
+++ mycarapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import MyCar
+
 
 # Create your views here.
+
+def home(requset):
+    site = MyCar.objects.get(pk = 2)
+    return render(requset, 'front/pages/home.html', {'site':site,})
+
+
+def about(requset):
+    site = MyCar.objects.get(pk = 2)
+    return render(requset, 'front/pages/about.html', {'site':site})
