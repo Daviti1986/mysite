e4e71ce34ad6904896061b0b29dea16f8453d46a
daviti1986 <davitpkhakadze86@gmail.com>
2020-05-28 13:43:39 +0400

    new_apps

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('mycarapp.urls')),
+    url(r'', include('suggestapp.urls')),
 ]
--- suggestapp/apps.py
+++ suggestapp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SuggestConfig(AppConfig):
-    name = 'suggest'
+    name = 'suggestapp'
